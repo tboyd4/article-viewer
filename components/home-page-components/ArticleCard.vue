cb6c6782dddfa409c7230f67acadc688fdcb2670
TylerBoyd <tboyd1898@gmail.com>
2020-11-30 13:27:59 -0600

    delete functionality added to each article

--- components/home-page-components/ArticleCard.vue
+++ components/home-page-components/ArticleCard.vue
@@ -1,28 +1,22 @@
 <template>
   <div>
-    <v-row align="center" justify="center">
+    <v-row v-for="article in articleData" :key="article.articleNumber" align="center" justify="center">
       <v-col cols="12" md="6">
         <v-card class="mx-auto my-12">
-          <v-img height="250" :src="image"></v-img>
+          <v-img height="250" :src="article.image"></v-img>
 
-          <v-card-title>{{ title }}</v-card-title>
+          <v-card-title>{{ article.title }}</v-card-title>
 
           <v-card-text>
             <div>
-              {{ desc }}
+              {{ article.desc }}
             </div>
           </v-card-text>
 
           <v-divider class="mx-4"></v-divider>
 
           <v-card-actions>
-            <v-btn
-              class="my-2 mx-1"
-              color="info"
-              outlined
-              rounded
-              @click="readPost"
-            >
+            <v-btn class="my-2 mx-1" color="info" outlined rounded>
               Read More
             </v-btn>
             <v-btn
@@ -30,11 +24,13 @@
               color="warning"
               outlined
               rounded
-              @click="deletePost"
+              @click="deleteArticle(article.articleNumber)"
             >
               Delete Article
             </v-btn>
           </v-card-actions>
+          <v-spacer></v-spacer>
+          <p>{{ articleNumber }}</p>
         </v-card>
       </v-col>
     </v-row>
@@ -43,15 +39,57 @@
 
 <script>
 export default {
-  data() {},
-  props: ['title', 'desc', 'image'],
+  data() {
+    return {
+      articleData: [
+        {
+          articleNumber: 1,
+          title: 'Drones, The Future of Food Delivery',
+          description:
+            'Sometime in the future, a drone flown by a man in a building in Idaho will be delivering your DoorDash. That time is coming soon. ',
+          image: '/image1.jpg',
+        },
+        {
+          articleNumber: 2,
+          title: 'Programming Is Cool',
+          description:
+            'Programming is great because you can do fun stuff and get paid pretty darn good for it. Read this article to find out more',
+          image: '/image2.jpg',
+        },
+        {
+          articleNumber: 3,
+          title: 'Are Co-Working Spaces Actually Productive?',
+          description:
+            'Yes. They are super awesome and cool, especially if they are in the middle of downtown KC near a Messenger Coffee.',
+          image: '/image3.jpg',
+        },
+        {
+          articleNumber: 4,
+          title: 'Should you get the new M1 MacBook Pro?',
+          description:
+            "Probably should. It is super fast and you can build and export code files so much faster. It's pretty cool.",
+          image: '/image4.jpg',
+        },
+        {
+          articleNumber: 5,
+          title: 'What is the future of Tech?',
+          description:
+            'Fast growing tech consultancy company Uhray is the future of tech. Enough said.',
+          image: '/image5.jpg',
+        },
+      ],
+    }
+  },
   methods: {
-      readPost() {
-          console.log('reading post')
-      },
-      deletePost() {
-          console.log('deleting post')
-      }
+    deleteArticle(articleNumber) {
+        console.log('deleting ' + articleNumber)
+        for (let i = 0; i < this.articleData.length; i++) {
+            if (this.articleData[i].articleNumber === articleNumber) {
+                console.log('deleted ' + this.articleData[i].title)
+                this.articleData.splice(i, 1)
+            }
+        }
+    }
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,46 +6,13 @@
       </v-col>
     </v-row>
 
-    <div v-for="article in articleData" :key="article.title">
-      <article-card :title="article.title" :desc="article.description" :image="article.image"></article-card>
-    </div>
+    <article-card></article-card>
   </div>
 </template>
 
 <script>
 import ArticleCard from '../components/home-page-components/ArticleCard.vue'
 export default {
-  data () {
-    return {
-      articleData: [
-        {
-          title: "Programming Is Cool",
-          description: "Programming is great because you can do fun stuff and get paid pretty darn good for it. Read this article to find out more",
-          image: "/image1.jpg"
-        },
-        {
-          title: "Programming Is Cool",
-          description: "Programming is great because you can do fun stuff and get paid pretty darn good for it. Read this article to find out more",
-          image: "/image2.jpg"
-        },
-        {
-          title: "Programming Is Cool",
-          description: "Programming is great because you can do fun stuff and get paid pretty darn good for it. Read this article to find out more",
-          image: "/image3.jpg"
-        },
-        {
-          title: "Programming Is Cool",
-          description: "Programming is great because you can do fun stuff and get paid pretty darn good for it. Read this article to find out more",
-          image: "/image4.jpg"
-        },
-        {
-          title: "Programming Is Cool",
-          description: "Programming is great because you can do fun stuff and get paid pretty darn good for it. Read this article to find out more",
-          image: "/image5.jpg"
-        }
-      ]
-    }
-  },
   components: {
     ArticleCard,
   },
